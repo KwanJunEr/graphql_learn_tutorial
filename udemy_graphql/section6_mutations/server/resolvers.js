1ac9330ef0c1872f0ba3d2e0eb0410158d480f7f
KwanJunEr <jonastheitguy@gmail.com>
2025-12-25 16:07:59 +0800

    completed section6

--- udemy_graphql/section6_mutations/server/resolvers.js
+++ udemy_graphql/section6_mutations/server/resolvers.js
@@ -1,6 +1,6 @@
 import { GraphQLError } from 'graphql';
 import { getCompany } from './db/companies.js';
-import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
+import { deleteJob, getJob, getJobs, getJobsByCompany, updateJob } from './db/jobs.js';
 import { createJob } from './db/jobs.js';
 
 export const resolvers = {
@@ -27,8 +27,11 @@ export const resolvers = {
     createJob: (_root, {input: {title, description}})=>{
       const companyId = 'FjcJCHJALA4i'
       return  createJob({companyId, title, description})
-    }
-     
+    },
+    deleteJob: (_root, {id}) => deleteJob(id),
+    updateJob: (_root, {input: {id, title, description}})=>{
+      return updateJob({id, title, description})
+    },
   },
 
   Company: {
